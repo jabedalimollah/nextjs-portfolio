88f643987fbf73c60758f061135291157c928bee
Jabed Ali Mollah <jabedalimollah7@gmail.com>
2025-01-17 17:03:28 +0530

    add date and calendar in header section

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,9 @@ import React, { useEffect, useState } from "react";
 import { FiClock, FiSun } from "react-icons/fi";
 import { RiMoonClearLine } from "react-icons/ri";
 import Clock from "react-clock";
+import Calendar from "react-calendar";
 import "react-clock/dist/Clock.css";
+import "react-calendar/dist/Calendar.css";
 import {
   Dialog,
   DialogContent,
@@ -16,23 +18,39 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { useDispatch, useSelector } from "react-redux";
+import { FaCalendarAlt } from "react-icons/fa";
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 const Header: React.FC = () => {
   const isDarkmode = useSelector((state: any) => state.theme.darkmode);
-
+  const [date, setDate] = useState<Value>(new Date());
   const [time, setTime] = useState(new Date());
+  const [isClock, setIsClock] = useState<boolean>(true);
   let timeString = new Date().toLocaleTimeString();
   const [ctime, setCtime] = useState(timeString);
   const dispatch = useDispatch();
 
-  setInterval(() => {
-    timeString = new Date().toLocaleTimeString();
-    setCtime(timeString);
-  }, 1000);
+  // setInterval(() => {
+  //   timeString = new Date().toLocaleTimeString();
+  //   setCtime(timeString);
+  // }, 1000);
+  const handleDateChange = () => {
+    setDate(date);
+  };
   useEffect(() => {
-    setInterval(() => {
+    const interval = setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+    return () => clearInterval(interval); // Clear interval on unmount
   }, []);
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     setTime(new Date());
+  //   }, 1000);
+  // }, []);
   return (
     <header className="h-11 w-full flex justify-between items-center bg-purple-700 sticky z-50 top-0 left-0">
       <Link href={"/"} className="h-[60%]">
@@ -44,7 +62,23 @@ const Header: React.FC = () => {
       {/* ===================== Clock Start ============================= */}
       <Dialog>
         <DialogTrigger asChild>
-          <button className={`text-white font-mono`}>{ctime}</button>
+          <div className={`flex gap-x-6`}>
+            <button
+              onClick={() => setIsClock(true)}
+              className={`text-white font-mono hover:text-purple-200`}
+            >
+              {/* {ctime} */}
+              {time.toLocaleTimeString()}
+            </button>
+            <button
+              onClick={() => setIsClock(false)}
+              className={`text-white font-mono hover:text-purple-200`}
+            >
+              {/* {ctime} */}
+              {/* {time.toLocaleDateString()} */}
+              {time.getDate()}/{time.getMonth() + 1}/{time.getFullYear()}
+            </button>
+          </div>
         </DialogTrigger>
         <DialogContent
           className={`sm:max-w-[425px]s ${
@@ -52,42 +86,76 @@ const Header: React.FC = () => {
           }`}
         >
           <DialogHeader>
-            <DialogTitle className={`flex gap-x-1 my-2 `}>
-              <FiClock />
-              Clock
+            <DialogTitle className={`w-full flex gap-x-2 my-2 `}>
+              <button
+                onClick={() => setIsClock(true)}
+                className={`w-[50%]  ${
+                  isDarkmode
+                    ? `${isClock ? "bg-slate-900" : "bg-slate-700 "}`
+                    : `${isClock ? "bg-slate-200" : "bg-slate-100 "}`
+                } p-4  flex justify-center gap-x-1 my-2 `}
+              >
+                <FiClock />
+                Clock
+              </button>
+              <button
+                onClick={() => setIsClock(false)}
+                className={`w-[50%]  ${
+                  isDarkmode
+                    ? `${isClock ? "bg-slate-700" : "bg-slate-900 "}`
+                    : `${isClock ? "bg-slate-100" : "bg-slate-200 "}`
+                } p-4  flex justify-center gap-x-1 my-2 `}
+              >
+                <FaCalendarAlt />
+                Calendar
+              </button>
             </DialogTitle>
             <DialogDescription
               // className={`${isDarkmode ? "text-white" : "text-black"}`}
-              className={`${
-                isDarkmode ? "text-purple-500" : "text-purple-700"
+              className={`w-full text-center font-serif text-lg ${
+                isDarkmode ? "text-purple-200" : "text-purple-700"
               }`}
             >
-              Check the time on the clock.
+              {isClock
+                ? "Check the time on the clock."
+                : "Check the date on the calendar."}
             </DialogDescription>
           </DialogHeader>
-          <div className="grid gap-4 py-4">
+          {/* <div className="grid gap-4 py-4">
             <div className="grid grid-cols-4 items-center gap-4"></div>
             <div className="grid grid-cols-4 items-center gap-4"></div>
-          </div>
-          <div className={`w-full flex flex-col items-center justify-center`}>
-            <Clock
-              value={time}
-              size={300}
-              renderNumbers={true}
-              className={`border-4 ${
-                isDarkmode
-                  ? " border-purple-500 bg-purple-100 text-black"
-                  : " border-purple-600 text-black"
-              } rounded-full`}
-            />
-            <div
-              className={`my-4 border shadow-md ${
-                isDarkmode ? "shadow-slate-900" : "shadow-slate-400"
-              }  border-purple-600 text-purple-500 hover:bg-purple-600 hover:text-white  cursor-pointer font-semibold font-mono px-4 py-2 rounded-md`}
-            >
-              {ctime}
+          </div> */}
+          {isClock ? (
+            <div className={`w-full flex flex-col items-center justify-center`}>
+              <Clock
+                value={time}
+                size={300}
+                renderNumbers={true}
+                className={`border-4 ${
+                  isDarkmode
+                    ? " border-purple-500 bg-purple-100 text-purple-600"
+                    : " border-purple-600 text-purple-600"
+                } rounded-full`}
+              />
+              <div
+                className={`my-4 border shadow-md ${
+                  isDarkmode ? "shadow-slate-900" : "shadow-slate-400"
+                }  border-purple-600 text-purple-500 hover:bg-purple-600 hover:text-white  cursor-pointer font-semibold font-mono px-4 py-2 rounded-md`}
+              >
+                {/* {ctime} */}
+                {time.toLocaleTimeString()}
+              </div>
             </div>
-          </div>
+          ) : (
+            <div className={`w-full flex flex-col items-center justify-center`}>
+              <Calendar
+                onChange={handleDateChange}
+                value={date}
+                className={`text-black rounded-md`}
+              />
+            </div>
+          )}
+
           <DialogFooter></DialogFooter>
         </DialogContent>
       </Dialog>
