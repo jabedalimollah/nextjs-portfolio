1748dcc498aad656dd3d47e3ef5974264261669e
Jabed Ali Mollah <jabedalimollah7@gmail.com>
2025-01-22 15:58:09 +0530

    fix some bugs

--- src/components/animatedPicture.tsx
+++ src/components/animatedPicture.tsx
@@ -1,14 +1,14 @@
-"use client";
-import React from "react";
-import Lottie from "lottie-react";
+// "use client";
+// import React from "react";
+// import Lottie from "lottie-react";
 
-interface AnimationString {
-  pathString: string | object;
-}
-const AnimatedPicture: React.FC<AnimationString> = ({ pathString }) => {
-  return (
-    <Lottie animationData={pathString} loop={true} className={`w-[80%]`} />
-  );
-};
+// interface AnimationString {
+//   pathString: string | object;
+// }
+// const AnimatedPicture: React.FC<AnimationString> = ({ pathString }) => {
+//   return (
+//     <Lottie animationData={pathString} loop={true} className={`w-[80%]`} />
+//   );
+// };
 
-export default AnimatedPicture;
+// export default AnimatedPicture;
